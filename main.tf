f3e7456d68f6ffcecd0fda963de4225a73dd60d9
Kreyno <michael.wagener@neuefische.de>
2025-11-14 17:50:04 +0100

    commenting out bucket settings due to sandbox restrictions

--- main.tf
+++ main.tf
@@ -11,11 +11,11 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_s3_bucket" "wordpress-bucket" {
-  bucket = "deham24-wordpress-bucket-v2"
+# resource "aws_s3_bucket" "wordpress-bucket" {
+#   bucket = "deham24-wordpress-bucket-v2"
 
-  tags = {
-    Name        = "Webserver Bucket"
-    Environment = "Test"
-  }
-}
+#   tags = {
+#     Name        = "Webserver Bucket"
+#     Environment = "Test"
+#   }
+# }
